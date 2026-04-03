7feb5f4198b2871ff9a7bdfce7dac85616326cc8
delwarhossaincse-ship-it <delwar.hossain.cse@ulab.edu.bd>
2026-04-03 22:09:53 +0600

    payment system integration

--- Backend/src/routes/paymentRoutes.js
+++ Backend/src/routes/paymentRoutes.js
@@ -4,26 +4,15 @@ const authMiddleware = require('../middleware/authMiddleware');
 const {
     initiatePayment,
     verifyPayment,
-    handleCallback,
+    handleWebhook,
     getPaymentStatus,
-    getPaymentHistory,
+    getPaymentHistory
 } = require('../controllers/paymentController');
 
-
 router.post('/initiate', initiatePayment);
-
-
 router.post('/verify', verifyPayment);
-
-
-
-router.post('/callback', handleCallback);
-
-
-router.get('/status/:transactionId', getPaymentStatus);
-
-
+router.post('/webhook', handleWebhook);
+router.get('/status/:invoiceId', getPaymentStatus);
 router.get('/history', authMiddleware, getPaymentHistory);
-
 
 module.exports = router;
