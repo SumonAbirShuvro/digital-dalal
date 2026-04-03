06833d34ccac8b9729c94525fc52b87f7e5661b5
delwarhossaincse-ship-it <delwar.hossain.cse@ulab.edu.bd>
2026-04-03 22:07:17 +0600

    Update payment.js

--- Backend/src/config/payment.js
+++ Backend/src/config/payment.js
@@ -1,6 +1,6 @@
 const paymentConfig = {
-    apiUrl: process.env.UDDOKTAPAY_API_URL || 'https://sandbox.uddoktapay.com',
-    apiKey: process.env.UDDOKTAPAY_API_KEY || '',
+    apiUrl: process.env.UDDOKTAPAY_API_URL || 'https://sandbox.uddoktapay.com/api/checkout-v2',
+    apiKey: process.env.UDDOKTAPAY_API_KEY || '982d381360a69d419689740d9f2e26ce36fb7a50',
     redirectUrl: process.env.UDDOKTAPAY_REDIRECT_URL || 'http://localhost:5173/payment/success',
     cancelUrl: process.env.UDDOKTAPAY_CANCEL_URL || 'http://localhost:5173/payment/cancel',
     webhookUrl: process.env.UDDOKTAPAY_WEBHOOK_URL || 'http://localhost:5000/api/payment/webhook',
