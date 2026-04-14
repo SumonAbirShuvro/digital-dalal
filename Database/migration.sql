84efaed806d6f13d51adb15efe0dd277db3dfb3f
UmmeSalma <umme.salma1.cse@ulab.edu.bd>
2026-04-14 21:19:50 +0600

    Delete Database directory

--- Database/migration.sql
+++
@@ -1,8 +0,0 @@
--- Run this in your MySQL database if current_address column doesn't exist
-
-ALTER TABLE users 
-ADD COLUMN IF NOT EXISTS current_address VARCHAR(255) DEFAULT NULL;
-
--- Make sure role column supports all three values
-ALTER TABLE users 
-MODIFY COLUMN role ENUM('citizen', 'admin', 'officer') DEFAULT 'citizen';
