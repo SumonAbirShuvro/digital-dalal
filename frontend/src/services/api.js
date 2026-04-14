788e31135f3afa8e1d2f433df4c11779e566977c
UmmeSalma <umme.salma1.cse@ulab.edu.bd>
2026-04-15 01:23:29 +0600

    Change API base URL from localhost to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://digital-dalal.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
